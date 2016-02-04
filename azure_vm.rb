27e50b5985b164bf8b6dc8f1182db3e96e9eaac4
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-02-04 21:58:58 +0000

    update

--- azure_vm.rb
+++ azure_vm.rb
@@ -6,16 +6,16 @@ def l
   ("a".."z").to_a[rand(26)]
 end
 
-json.gsub! /<MKV>/,   "mkv-#{time.hour}-#{time.min}"
-json.gsub! /<MKVID>/, "#{l}#{l}" # #{l}#{l}#{l}#{l}#{l}
+json.gsub! /<USR>/,   "mkv-#{time.hour}-#{time.min}"
+json.gsub! /<USRID>/, "#{l}#{l}" # #{l}#{l}#{l}#{l}#{l}
 
+File.open "./gen/azuredeploy.json", "w" do |f|
+  f.write json
+end
 
 puts json
 
-
-# File.open "./gen/azuredeploy.json", "w" do |f|
-#   puts json
-# end
-
-
+# Usage:
 # ruby azure_vm.rb | xsel -b
+#
+# or cat gen/azuredeploy.json and copy it to azure
